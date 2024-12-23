9839de9566901ed4c941794d4ea5ece3e29b1d59
Tatoafanasieva <Tamariafanasieva10@gmail.com>
2024-12-23 20:24:29 +0400

    level10

--- level10/homework/homework.py
+++ level10/homework/homework.py
@@ -2,9 +2,8 @@
 
 name = input("enter your name: ")
 print(name)
-age = (input("enter your age: "))
+age = int(input("enter your age: "))
 print(age)
-answer = input("გამარჯობა, " + name + "! "+ "შენ ხარ " + age + " წლის")
-print(answer)
+print("გამარჯობა, " + name + "! "+ "შენ ხარ " + str(age) + " წლის")
 
 
