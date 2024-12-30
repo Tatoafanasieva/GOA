a3a25b0a371c5c3d326f6a7b831a481d6f4ed08e
Tatoafanasieva <Tamariafanasieva10@gmail.com>
2024-12-30 19:15:14 +0400

    level 12

---
+++ level12/homework/homework1.py
@@ -0,0 +1,20 @@
+# 2) int, str და float ბრძანებებზე ჩამოწერეთ 5-5 მაგალითი 
+
+int1 = 5
+int2 = 6
+int3 = 7
+int4 = 8
+int5 = 9
+
+str1 = "hello"
+str2 = "what"
+str3 = "olla"
+str4 = "8"
+str5 = "15"
+
+float1 = 10.3
+float2 = 15.6
+float3 = 19.4
+float4 = 20.8
+float5 = 55.0
+
