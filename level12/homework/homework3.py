91966ecd53f89809c609231f7ccb0369e8c7f11a
Tatoafanasieva <Tamariafanasieva10@gmail.com>
2025-01-01 17:30:23 +0400

    level12.5

--- level12/homework/homework3.py
+++ level12/homework/homework3.py
@@ -1,6 +1,9 @@
 # 4) სამ ადამიანს შემოატანინეთ ასაკები და გამოითვალეთ ამ ასაკების საშუალო არითმეტიკული (ასაკების ჯამი / ადამიანების რაოდენობა)
 
-prsn1 = int(input("30"))
-prsn2 = int(input("50"))
-prsn3 = int(input("10"))
+prsn1 = int(input("enter your age: "))
+print(prsn1)
+prsn2 = int(input("enter your age: "))
+print(prsn2)
+prsn3 = int(input("enter your age: "))
+print(prsn2)
 print(prsn1 + prsn2 + prsn3 // 3)
